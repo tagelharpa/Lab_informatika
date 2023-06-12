7ccd1d2301e55782b1b44c50c9f00eebfe376145
tagelharpa <76700337+tagelharpa@users.noreply.github.com>
2023-06-12 21:42:33 +0300

    lab_7

---
+++ Лабораторная_7/Лабораторная_7.cpp
@@ -0,0 +1,26 @@
+﻿#include "Vector.h"
+#include <iostream>
+#include "Time.h"
+using namespace std;
+void main()
+{
+
+
+	Time t;
+	cin >> t;
+	cout << t;
+	Vector<Time>A(5, t);
+	cin >> A;
+	cout << A << endl;
+	Vector <Time>B(10, t);
+	cout << B << endl;
+	B = A;
+	cout << B << endl;
+	cout << A[2] << endl;
+	cout << "size=" << A() << endl;
+	B = A + t;
+	cout << B << endl;
+	B = B + 532;
+	cout << B << endl;
+
+}
