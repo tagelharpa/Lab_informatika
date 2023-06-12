b12f1d58ba95a424d0c2bbdcc088c805d36e80e6
tagelharpa <76700337+tagelharpa@users.noreply.github.com>
2023-06-12 21:42:05 +0300

    lab_5

---
+++ Лабораторная_5/Лабораторная_5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "Pair.h"
+#include "Fraction.h"
+#include "Vector.h"
+#include "object.h"
+using namespace std;
+
+void main()
+{
+	Vector v(5);//вектор из 5 элементов
+	Pair a;//объект класса Car
+	cin >> a;
+	Fraction b;// объект класса Lorry
+	cin >> b;
+	Object* p = &a;//ставим указатель на объект класса Car
+	v.Add(p);//добавляем объект в вектор
+	p = &b;//ставим указатель на объект класса Lorry
+	v.Add(p); //добавляем объект в вектор
+	cout << v;//вывод вектора
+
+}
